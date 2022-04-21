0c81a82fccf572cd57143b27cfb8ceb66301cbf3
eddd9 <104097855+eddd9@users.noreply.github.com>
2022-04-20 18:03:16 -0700

    Update pipeline.cpp

--- pipeline.cpp
+++ pipeline.cpp
@@ -85,7 +85,7 @@ bool Pipeline::hasDependency(void) {
 
 	// Checks if dependency exist between Decode stage and Exec, Mem stage
 	// We assume the register file can read/write in the same cycle so no data dependency exist with RAW dependency if an instruction is in Decode and WB.
-	for(int i = EXEC; i < WB; i++) {
+	for(int i = EXEC; i <= WB; i++) {
 
 		if( pipeline[i].inst == NULL )
 			continue;		
@@ -96,7 +96,19 @@ bool Pipeline::hasDependency(void) {
 		if( (pipeline[i].inst->dest != -1) && 
 		    (pipeline[i].inst->dest == pipeline[DECODE].inst->src1 ||
 		     pipeline[i].inst->dest == pipeline[DECODE].inst->src2) ) {
-			return true;
+			//EXEC/MEM-->DECODE
+			if (i - DECODE <= 2) {
+				if (i == 2 && (pipeline[i].inst->type == SW || pipeline[i].inst->type == LW))
+					return true;
+				else
+					return false;
+			}
+			//MEM/WB-->DECODE
+			else if (i - DECODE == 3 || i - DECODE == 2)
+				return false;
+			else
+				return false
+
 		}
 
 	}
